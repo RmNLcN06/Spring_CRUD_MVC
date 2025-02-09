e7e340ef11d10135a4bb6b7368d726c605cb0942
RmNLcN06 <romain.luciano@orange.fr>
2025-02-09 21:23:09 +0100

    Initial commit

---
+++ src/main/java/com/rmnlcn/Spring_CRUD_MVC/SpringCrudMvcApplication.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.Spring_CRUD_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCrudMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rmnlcn/Spring_CRUD_MVC/SpringCrudMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.Spring_CRUD_MVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCrudMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
