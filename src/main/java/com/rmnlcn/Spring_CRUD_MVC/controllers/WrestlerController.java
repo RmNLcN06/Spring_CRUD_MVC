395f73fa3eb4adc90377764c1b4becd228e78c77
RmNLcN06 <romain.luciano@orange.fr>
2025-02-10 22:01:20 +0100

    Process form data to save wrestler

--- src/main/java/com/rmnlcn/Spring_CRUD_MVC/controllers/WrestlerController.java
+++ src/main/java/com/rmnlcn/Spring_CRUD_MVC/controllers/WrestlerController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -43,5 +45,15 @@ public class WrestlerController {
         theModel.addAttribute("wrestler", theWrestler);
 
         return "wrestlers/wrestler-form";
+    }
+
+    @PostMapping("/save")
+    public String saveWrestler(@ModelAttribute("wrestler") Wrestler theWrestler) {
+
+        // save the wrestler
+        wrestlerService.save(theWrestler);
+
+        // use a redirect to prevent duplicate submissions
+        return "redirect:/wrestlers/list";
     }
 }
