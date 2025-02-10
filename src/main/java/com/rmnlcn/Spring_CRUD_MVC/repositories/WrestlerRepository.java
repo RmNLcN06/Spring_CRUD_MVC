0a20f2e9e2b9afb679d5bff07da7c77e3e890999
RmNLcN06 <romain.luciano@orange.fr>
2025-02-10 22:11:52 +0100

    Sort by last name in wrestler list by adding custom method to JPA Repository

--- src/main/java/com/rmnlcn/Spring_CRUD_MVC/repositories/WrestlerRepository.java
+++ src/main/java/com/rmnlcn/Spring_CRUD_MVC/repositories/WrestlerRepository.java
@@ -3,5 +3,10 @@ package com.rmnlcn.Spring_CRUD_MVC.repositories;
 import com.rmnlcn.Spring_CRUD_MVC.entities.Wrestler;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface WrestlerRepository extends JpaRepository<Wrestler, Integer> {
+
+    // add method to sort by last name
+    public List<Wrestler> findAllByOrderByLastNameAsc();
 }
