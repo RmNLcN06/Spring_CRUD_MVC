3503a2fc8915cc0dcd32873d53203db2eddf6a55
RmNLcN06 <romain.luciano@orange.fr>
2025-02-12 20:46:52 +0100

    Adding users, passwords and roles for Spring Security

--- src/main/java/com/rmnlcn/Spring_CRUD_MVC/securities/ProjectSecurityConfig.java
+++ src/main/java/com/rmnlcn/Spring_CRUD_MVC/securities/ProjectSecurityConfig.java
@@ -1,7 +1,34 @@
 package com.rmnlcn.Spring_CRUD_MVC.securities;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class ProjectSecurityConfig {
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsManager() {
+        UserDetails lilas = User.builder()
+                .username("lilas")
+                .password("{noop}test123")
+                .roles("SIMPLE_USER")
+                .build();
+
+        UserDetails romain = User.builder()
+                .username("romain")
+                .password("{noop}test123")
+                .roles("SIMPLE_USER", "REGISTERED_USER")
+                .build();
+
+        UserDetails isabelle = User.builder()
+                .username("isabelle")
+                .password("{noop}test123")
+                .roles("SIMPLE_USER", "REGISTERED_USER", "ADMIN_USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(lilas, romain, isabelle);
+    }
 }
