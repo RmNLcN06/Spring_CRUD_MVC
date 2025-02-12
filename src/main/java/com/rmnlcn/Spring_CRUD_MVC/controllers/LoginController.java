785544a7f363117f78639cdd21ff15329067dca2
RmNLcN06 <romain.luciano@orange.fr>
2025-02-12 22:35:36 +0100

    Develop controller to show the custom login form

---
+++ src/main/java/com/rmnlcn/Spring_CRUD_MVC/controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.rmnlcn.Spring_CRUD_MVC.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/showLoginPage")
+    public String showLoginPage() {
+        return "wrestlers/login-page";
+    }
+}
