994bee52ad19b377479bdb6b81d68869e42c5a59
RmNLcN06 <romain.luciano@orange.fr>
2025-02-17 15:53:02 +0100

    Update Srping Security Configuration to use JDBC

--- src/main/java/com/rmnlcn/Spring_CRUD_MVC/securities/ProjectSecurityConfig.java
+++ src/main/java/com/rmnlcn/Spring_CRUD_MVC/securities/ProjectSecurityConfig.java
@@ -3,14 +3,20 @@ package com.rmnlcn.Spring_CRUD_MVC.securities;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
+/*import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;*/
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class ProjectSecurityConfig {
 
+    /*
     @Bean
     public InMemoryUserDetailsManager userDetailsManager() {
         UserDetails lilas = User.builder()
@@ -32,6 +38,13 @@ public class ProjectSecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(lilas, romain, isabelle);
+    }
+     */
+
+    // Add support for JDBC
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
